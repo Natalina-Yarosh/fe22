267d269b0822772044c3db6cc842a64a7763fce0
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-01-26 20:08:16 +0300

    edit burger

--- landing_active_box/js/script.js
+++ landing_active_box/js/script.js
@@ -1,7 +1,6 @@
-
 var swiper = new Swiper(".testimonials__slider", {
    pagination: {
-     el: ".swiper-pagination",
-     clickable: true
+      el: ".swiper-pagination",
+      clickable: true
    },
- });
+});
