9e1c8d6264cd104bbc0e2eedb94e4275977a6329
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-01-29 21:53:42 +0300

    add burger using js

--- landing_active_box/js/script.js
+++ landing_active_box/js/script.js
@@ -1,3 +1,38 @@
+"use strict"
+
+// smooth scroll
+const menuHeader = document.querySelectorAll('.header-list__item[data-goto]');
+if (menuHeader.length > 0) {
+   menuHeader.forEach(headerlistitem => {
+      headerlistitem.addEventListener("click", onMenuHeaderClick);
+   });
+   function onMenuHeaderClick(e) {
+      const headerlistitem = e.target;
+      if (headerlistitem.dataset.goto && document.querySelector(headerlistitem.dataset.goto)) {
+         const gotoBlock = document.querySelector(headerlistitem.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYoffset - document.querySelector('header').offsetHeight;
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+         e.preventDefault();
+      }
+   }
+}
+
+// menu burger
+const headerBurger = document.querySelector('.header__burger');
+if (headerBurger) {
+   const headerMenu = document.querySelector('.header__menu');
+   headerBurger.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      headerBurger.classList.toggle('_active');
+      headerMenu.classList.toggle('_active');
+   })
+}
+
+// swiper
 var swiper = new Swiper(".testimonials-slider", {
    pagination: {
       el: ".swiper-pagination",
